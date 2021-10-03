45c0b1df355ec4d2b2a35935d039b616d635d485
GYashvant <gyashvantkumar@gmail.com>
2021-10-03 19:51:59 +0530

    project done

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,5 +1,4 @@
-let proceedBtn = document.getElementById("checkoutbot")
 
-proceedBtn.onclick = function(){
-  window.location.href = "payment.html"
+function proceedbtn(){
+  window.location.href = "/payment"
 }
